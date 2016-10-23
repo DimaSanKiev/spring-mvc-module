b4e2f78b6dd693409c5428b608d37fcdc5794074
Dmytro Burdyga <dmytro.burdyga@gmail.com>
2016-10-24 00:58:59 +0300

    add CustomerController

---
+++ src/main/java/guru/springframework/controller/CustomerController.java
@@ -0,0 +1,24 @@
+package guru.springframework.controller;
+
+import guru.springframework.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class CustomerController {
+
+    private CustomerService customerService;
+
+    @Autowired
+    public void setCustomerService(CustomerService customerService) {
+        this.customerService = customerService;
+    }
+
+    @RequestMapping("/customers")
+    public String getCustomersList(Model model) {
+        model.addAttribute("customers", customerService.listAllCustomers());
+        return "customers";
+    }
+}
