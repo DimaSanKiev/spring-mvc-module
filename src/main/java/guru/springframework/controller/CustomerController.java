cd71c00c8eb6a8d5e7be6e372c88e0d518a24673
Dmytro Burdyga <dmytro.burdyga@gmail.com>
2016-10-24 23:20:19 +0300

    Add customer template and getCustomerById method

--- src/main/java/guru/springframework/controller/CustomerController.java
+++ src/main/java/guru/springframework/controller/CustomerController.java
@@ -4,6 +4,7 @@ import guru.springframework.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -20,5 +21,11 @@ public class CustomerController {
     public String getCustomersList(Model model) {
         model.addAttribute("customers", customerService.listAllCustomers());
         return "customers";
+    }
+
+    @RequestMapping("/customer/{id}")
+    public String getCustomerById(@PathVariable Integer id, Model model) {
+        model.addAttribute("customer", customerService.getCustomerById(id));
+        return "customer";
     }
 }
