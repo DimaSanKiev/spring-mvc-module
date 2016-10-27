aed4493bb00e2375e22f352dce345bb064857fda
Dmytro Burdyga <dmytro.burdyga@gmail.com>
2016-10-27 23:54:09 +0300

    Add ProductControllerTest and test list() method

---
+++ src/test/java/guru/springframework/controller/ProductControllerTest.java
@@ -0,0 +1,55 @@
+package guru.springframework.controller;
+
+import guru.springframework.domain.Product;
+import guru.springframework.service.ProductService;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+public class ProductControllerTest {
+
+    @Mock   // Mockito Mock object
+    private ProductService productService;
+
+    @InjectMocks    // setups up controller, and injects mock objects into it
+    private ProductController productController;
+
+    private MockMvc mockMvc;
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this); // initializes controller and mocks
+        mockMvc = MockMvcBuilders.standaloneSetup(productController).build();
+    }
+
+    @Test
+    public void testList() throws Exception {
+        List<Product> products = new ArrayList<>();
+        products.add(new Product());
+        products.add(new Product());
+
+        // specific Mockito interaction, tell stub to return list of products
+        when(productService.listAll()).thenReturn((List) products); // need to strip generics to keep Mockito happy
+
+        mockMvc.perform(get("/product/list"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("product/list"))
+                .andExpect(model().attribute("products", hasSize(2)));
+    }
+
+    
+}
