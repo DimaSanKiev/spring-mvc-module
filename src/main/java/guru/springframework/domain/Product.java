0e2e198d3b7cd51e68d731abe1c48ced52f1ec33
Dmytro Burdyga <dmytro.burdyga@gmail.com>
2016-10-30 00:53:55 +0300

    Enable JPA optimistic locking by providing version property

--- src/main/java/guru/springframework/domain/Product.java
+++ src/main/java/guru/springframework/domain/Product.java
@@ -4,13 +4,19 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Version;
 import java.math.BigDecimal;
 
 @Entity
 public class Product implements DomainObject {
+
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
+
+    @Version
+    private Integer version;
+
     private String description;
     private BigDecimal price;
     private String imageUrl;
@@ -23,6 +29,14 @@ public class Product implements DomainObject {
     @Override
     public void setId(Integer id) {
         this.id = id;
+    }
+
+    public Integer getVersion() {
+        return version;
+    }
+
+    public void setVersion(Integer version) {
+        this.version = version;
     }
 
     public String getDescription() {
