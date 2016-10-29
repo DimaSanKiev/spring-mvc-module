c96e1ad6b586d9e2c200cb630154f65403f4001b
Dmytro Burdyga <dmytro.burdyga@gmail.com>
2016-10-30 00:56:58 +0300

    Convert the Customer object to a JPA Entity

--- src/main/java/guru/springframework/domain/Customer.java
+++ src/main/java/guru/springframework/domain/Customer.java
@@ -1,7 +1,21 @@
 package guru.springframework.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Version;
+
+@Entity
 public class Customer implements DomainObject {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
+
+    @Version
+    private Integer version;
+
     private String firstName;
     private String lastName;
     private String email;
@@ -20,6 +34,14 @@ public class Customer implements DomainObject {
     @Override
     public void setId(Integer id) {
         this.id = id;
+    }
+
+    public Integer getVersion() {
+        return version;
+    }
+
+    public void setVersion(Integer version) {
+        this.version = version;
     }
 
     public String getFirstName() {
