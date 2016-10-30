29da1e1e336fac244745b701ef796c755c00b46c
Dmytro Burdyga <dmytro.burdyga@gmail.com>
2016-10-30 16:47:15 +0200

    Add database population on startap

--- src/main/java/guru/springframework/SpringMvcApplication.java
+++ src/main/java/guru/springframework/SpringMvcApplication.java
@@ -2,11 +2,18 @@ package guru.springframework;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringMvcApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringMvcApplication.class, args);
+        ApplicationContext ctx = SpringApplication.run(SpringMvcApplication.class, args);
+
+//        for (String name : ctx.getBeanDefinitionNames()){
+//            System.out.println(name);
+//        }
+//        System.out.println("***** Bean Count *****");
+//        System.out.println(ctx.getBeanDefinitionCount());
     }
 }
