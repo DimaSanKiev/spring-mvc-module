898a8a56e214818e05541e882b5d2018c5ae26e4
Dmytro Burdyga <dmytro.burdyga@gmail.com>
2016-10-30 21:06:30 +0200

    Add loadCustomers() method in SpringJpaBootstrap class to populate database with customers on startup

--- src/main/java/guru/springframework/bootstrap/SpringJpaBootstrap.java
+++ src/main/java/guru/springframework/bootstrap/SpringJpaBootstrap.java
@@ -1,6 +1,8 @@
 package guru.springframework.bootstrap;
 
+import guru.springframework.domain.Customer;
 import guru.springframework.domain.Product;
+import guru.springframework.service.CustomerService;
 import guru.springframework.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationListener;
@@ -13,15 +15,22 @@ import java.math.BigDecimal;
 public class SpringJpaBootstrap implements ApplicationListener<ContextRefreshedEvent> {
 
     private ProductService productService;
+    private CustomerService customerService;
 
     @Autowired
     public void setProductService(ProductService productService) {
         this.productService = productService;
     }
 
+    @Autowired
+    public void setCustomerService(CustomerService customerService) {
+        this.customerService = customerService;
+    }
+
     @Override
     public void onApplicationEvent(ContextRefreshedEvent event) {
         loadProducts();
+        loadCustomers();
     }
 
     private void loadProducts() {
@@ -54,5 +63,67 @@ public class SpringJpaBootstrap implements ApplicationListener<ContextRefreshedE
         product5.setPrice(new BigDecimal("25.99"));
         product5.setImageUrl("http://example.com/product5");
         productService.saveOrUpdate(product5);
+    }
+
+    private void loadCustomers() {
+        Customer customer1 = new Customer();
+        customer1.setFirstName("Alberta");
+        customer1.setLastName("Shultz");
+        customer1.setEmail("AlbertaGShultz@jourrapide.com");
+        customer1.setPhoneNumber("732-619-7166");
+        customer1.setAddressLineOne("3136 Webster Street");
+        customer1.setAddressLineTwo("");
+        customer1.setCity("Teterboro");
+        customer1.setState("NJ");
+        customer1.setZipCode("07608");
+        customerService.saveOrUpdate(customer1);
+
+        Customer customer2 = new Customer();
+        customer2.setFirstName("Tami");
+        customer2.setLastName("Cabezas");
+        customer2.setEmail("TamiJCabezas@jourrapide.com");
+        customer2.setPhoneNumber("205-271-9661");
+        customer2.setAddressLineOne("2525 Retreat Avenue");
+        customer2.setAddressLineTwo("");
+        customer2.setCity("Birmingham");
+        customer2.setState("AL");
+        customer2.setZipCode("35203");
+        customerService.saveOrUpdate(customer2);
+
+        Customer customer3 = new Customer();
+        customer3.setFirstName("William");
+        customer3.setLastName("Philip");
+        customer3.setEmail("WilliamGPhilip@dayrep.com");
+        customer3.setPhoneNumber("214-225-1154");
+        customer3.setAddressLineOne("482 Fancher Drive");
+        customer3.setAddressLineTwo("");
+        customer3.setCity("Dallas");
+        customer3.setState("TX");
+        customer3.setZipCode("75201");
+        customerService.saveOrUpdate(customer3);
+
+        Customer customer4 = new Customer();
+        customer4.setFirstName("Paul");
+        customer4.setLastName("Heslin");
+        customer4.setEmail("PaulCHeslin@armyspy.com");
+        customer4.setPhoneNumber("406-721-8674");
+        customer4.setAddressLineOne("2479 Tibbs Avenue");
+        customer4.setAddressLineTwo("");
+        customer4.setCity("Missoula");
+        customer4.setState("MT");
+        customer4.setZipCode("59802");
+        customerService.saveOrUpdate(customer4);
+
+        Customer customer5 = new Customer();
+        customer5.setFirstName("Douglas");
+        customer5.setLastName("Scott");
+        customer5.setEmail("DouglasLScott@jourrapide.com");
+        customer5.setPhoneNumber("540-453-6394");
+        customer5.setAddressLineOne("4471 Jehovah Drive");
+        customer5.setAddressLineTwo("");
+        customer5.setCity("Harrisonburg");
+        customer5.setState("VA");
+        customer5.setZipCode("22801");
+        customerService.saveOrUpdate(customer5);
     }
 }
