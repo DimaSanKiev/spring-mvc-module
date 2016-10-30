9b805c55a3d0189668610b5dfeb24c098af54ae4
Dmytro Burdyga <dmytro.burdyga@gmail.com>
2016-10-30 21:10:52 +0200

    Add tests

---
+++ src/test/java/guru/springframework/service/CustomerServiceJpaDaoImplTest.java
@@ -0,0 +1,33 @@
+package guru.springframework.service;
+
+import guru.springframework.domain.Customer;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest
+public class CustomerServiceJpaDaoImplTest {
+
+    private CustomerService customerService;
+
+    @Autowired
+    public void setCustomerService(CustomerService customerService) {
+        this.customerService = customerService;
+    }
+
+    @Test
+    public void testList() throws Exception {
+        List<Customer> customers = (List<Customer>) customerService.listAll();
+
+        assertNotNull(customers);
+        assertEquals(5, customers.size());
+    }
+}
--- src/test/java/guru/springframework/service/ProductServiceJpaDaoImplTest.java
+++ src/test/java/guru/springframework/service/ProductServiceJpaDaoImplTest.java
@@ -8,7 +8,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import java.math.BigDecimal;
 import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest
@@ -28,4 +32,11 @@ public class ProductServiceJpaDaoImplTest {
         assert products.size() == 5;
     }
 
+    @Test
+    public void testShow() throws Exception {
+        Product product = productService.getById(1);
+        assertNotNull(product);
+        assertEquals("Product 1", product.getDescription());
+        assertEquals(new BigDecimal("12.99"), product.getPrice());
+    }
 }
