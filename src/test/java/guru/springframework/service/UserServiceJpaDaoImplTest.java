4f1f1517d26421f6c56552bbecdc5aeb25b5517d
Dmytro Burdyga <dmytro.burdyga@gmail.com>
2016-11-01 00:03:39 +0200

    Add UserServiceTest testSaveOrUpdate method

---
+++ src/test/java/guru/springframework/service/UserServiceJpaDaoImplTest.java
@@ -0,0 +1,46 @@
+package guru.springframework.service;
+
+import guru.springframework.domain.User;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest
+@ActiveProfiles("jpadao")
+public class UserServiceJpaDaoImplTest {
+
+    private UserService userService;
+    private ProductService productService;
+
+    @Autowired
+    public void setUserService(UserService userService) {
+        this.userService = userService;
+    }
+
+    @Autowired
+    public void setProductService(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @Test
+    public void testSaveOrUpdate() throws Exception {
+        User user = new User();
+        user.setUsername("testusername");
+        user.setPassword("testpassword");
+
+        User savedUser = userService.saveOrUpdate(user);
+
+        assert savedUser.getId() != null;
+        assert savedUser.getEncryptedPassword() != null;
+
+        System.out.println("Encrypted password: " + savedUser.getEncryptedPassword());
+    }
+
+    
+}
