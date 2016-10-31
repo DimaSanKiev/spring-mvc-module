fa2563b7e312e4dce2af5ceedb9243d869d5d34b
Dmytro Burdyga <dmytro.burdyga@gmail.com>
2016-10-31 23:30:22 +0200

    Make OneToOne bidirectional relationships

--- src/main/java/guru/springframework/domain/User.java
+++ src/main/java/guru/springframework/domain/User.java
@@ -21,6 +21,9 @@ public class User implements DomainObject {
 
     private Boolean enabled = true;
 
+    @OneToOne(cascade = {CascadeType.MERGE, CascadeType.PERSIST})
+    private Customer customer;
+
     @Override
     public Integer getId() {
         return id;
@@ -69,5 +72,14 @@ public class User implements DomainObject {
 
     public void setEnabled(Boolean enabled) {
         this.enabled = enabled;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+        customer.setUser(this);
     }
 }
