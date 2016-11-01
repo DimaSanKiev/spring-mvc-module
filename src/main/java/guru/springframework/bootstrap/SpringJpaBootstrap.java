e51381961bc0e4123122c76959f1482135167528
Dmytro Burdyga <dmytro.burdyga@gmail.com>
2016-11-02 01:40:59 +0200

    Update SpringJpaBootstrap

--- src/main/java/guru/springframework/bootstrap/SpringJpaBootstrap.java
+++ src/main/java/guru/springframework/bootstrap/SpringJpaBootstrap.java
@@ -1,22 +1,23 @@
 package guru.springframework.bootstrap;
 
-import guru.springframework.domain.Address;
-import guru.springframework.domain.Customer;
-import guru.springframework.domain.Product;
+import guru.springframework.domain.*;
+import guru.springframework.domain.enums.OrderStatus;
 import guru.springframework.service.CustomerService;
 import guru.springframework.service.ProductService;
+import guru.springframework.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 @Component
 public class SpringJpaBootstrap implements ApplicationListener<ContextRefreshedEvent> {
 
     private ProductService productService;
-    private CustomerService customerService;
+    private UserService userService;
 
     @Autowired
     public void setProductService(ProductService productService) {
@@ -24,14 +25,16 @@ public class SpringJpaBootstrap implements ApplicationListener<ContextRefreshedE
     }
 
     @Autowired
-    public void setCustomerService(CustomerService customerService) {
-        this.customerService = customerService;
+    public void setUserService(UserService userService) {
+        this.userService = userService;
     }
 
     @Override
     public void onApplicationEvent(ContextRefreshedEvent event) {
         loadProducts();
-        loadCustomers();
+        loadUsersAndCustomers();
+        loadCarts();
+        loadOrderHistory();
     }
 
     private void loadProducts() {
@@ -66,7 +69,10 @@ public class SpringJpaBootstrap implements ApplicationListener<ContextRefreshedE
         productService.saveOrUpdate(product5);
     }
 
-    private void loadCustomers() {
+    private void loadUsersAndCustomers() {
+        User user1 = new User();
+        user1.setUsername("user1");
+        user1.setPassword("password");
         Customer customer1 = new Customer();
         customer1.setFirstName("Alberta");
         customer1.setLastName("Shultz");
@@ -78,8 +84,12 @@ public class SpringJpaBootstrap implements ApplicationListener<ContextRefreshedE
         customer1.getBillingAddress().setCity("Teterboro");
         customer1.getBillingAddress().setState("NJ");
         customer1.getBillingAddress().setZipCode("07608");
-        customerService.saveOrUpdate(customer1);
+        user1.setCustomer(customer1);
+        userService.saveOrUpdate(user1);
 
+        User user2 = new User();
+        user2.setUsername("user2");
+        user2.setPassword("password");
         Customer customer2 = new Customer();
         customer2.setFirstName("Tami");
         customer2.setLastName("Cabezas");
@@ -91,8 +101,12 @@ public class SpringJpaBootstrap implements ApplicationListener<ContextRefreshedE
         customer2.getBillingAddress().setCity("Birmingham");
         customer2.getBillingAddress().setState("AL");
         customer2.getBillingAddress().setZipCode("35203");
-        customerService.saveOrUpdate(customer2);
+        user2.setCustomer(customer2);
+        userService.saveOrUpdate(user2);
 
+        User user3 = new User();
+        user3.setUsername("user3");
+        user3.setPassword("password");
         Customer customer3 = new Customer();
         customer3.setFirstName("William");
         customer3.setLastName("Philip");
@@ -104,8 +118,12 @@ public class SpringJpaBootstrap implements ApplicationListener<ContextRefreshedE
         customer3.getBillingAddress().setCity("Dallas");
         customer3.getBillingAddress().setState("TX");
         customer3.getBillingAddress().setZipCode("75201");
-        customerService.saveOrUpdate(customer3);
+        user3.setCustomer(customer3);
+        userService.saveOrUpdate(user3);
 
+        User user4 = new User();
+        user4.setUsername("user4");
+        user4.setPassword("password");
         Customer customer4 = new Customer();
         customer4.setFirstName("Paul");
         customer4.setLastName("Heslin");
@@ -117,8 +135,12 @@ public class SpringJpaBootstrap implements ApplicationListener<ContextRefreshedE
         customer4.getBillingAddress().setCity("Missoula");
         customer4.getBillingAddress().setState("MT");
         customer4.getBillingAddress().setZipCode("59802");
-        customerService.saveOrUpdate(customer4);
+        user4.setCustomer(customer4);
+        userService.saveOrUpdate(user4);
 
+        User user5 = new User();
+        user5.setUsername("user5");
+        user5.setPassword("password");
         Customer customer5 = new Customer();
         customer5.setFirstName("Douglas");
         customer5.setLastName("Scott");
@@ -130,6 +152,39 @@ public class SpringJpaBootstrap implements ApplicationListener<ContextRefreshedE
         customer5.getBillingAddress().setCity("Harrisonburg");
         customer5.getBillingAddress().setState("VA");
         customer5.getBillingAddress().setZipCode("22801");
-        customerService.saveOrUpdate(customer5);
+        user5.setCustomer(customer5);
+        userService.saveOrUpdate(user5);
+    }
+
+    private void loadCarts() {
+        List<User> users = (List<User>) userService.listAll();
+        List<Product> products = (List<Product>) productService.listAll();
+
+        users.forEach(user -> {
+            user.setCart(new Cart());
+            CartDetail cartDetail = new CartDetail();
+            cartDetail.setProduct(products.get(0));
+            cartDetail.setQuantity(2);
+            user.getCart().addCartDetail(cartDetail);
+            userService.saveOrUpdate(user);
+        });
+    }
+
+    private void loadOrderHistory() {
+        List<User> users = (List<User>) userService.listAll();
+        List<Product> products = (List<Product>) productService.listAll();
+
+        users.forEach(user -> {
+            Order order = new Order();
+            order.setCustomer(user.getCustomer());
+            order.setOrderStatus(OrderStatus.SHIPPED);
+
+            products.forEach(product -> {
+                OrderDetail orderDetail = new OrderDetail();
+                orderDetail.setProduct(product);
+                orderDetail.setQuantity(1);
+                order.addtoOrderDetails(orderDetail);
+            });
+        });
     }
 }
