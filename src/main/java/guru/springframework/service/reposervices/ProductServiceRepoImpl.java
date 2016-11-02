1754146647fe46168740b50fab725c4292ffaf84
Dmytro Burdyga <dmytro.burdyga@gmail.com>
2016-11-03 01:09:52 +0200

    Add ProductServiceRepoImpl implementation with Profile "springdatajpa"

---
+++ src/main/java/guru/springframework/service/reposervices/ProductServiceRepoImpl.java
@@ -0,0 +1,45 @@
+package guru.springframework.service.reposervices;
+
+import guru.springframework.domain.Product;
+import guru.springframework.repository.ProductRepository;
+import guru.springframework.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@Profile({"springdatajpa", "jpadao"})
+public class ProductServiceRepoImpl implements ProductService{
+
+    private ProductRepository productRepository;
+
+    @Autowired
+    public void setProductRepository(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    @Override
+    public List<?> listAll() {
+        List<Product> products = new ArrayList<>();
+        productRepository.findAll().forEach(products::add);
+        return products;
+    }
+
+    @Override
+    public Product getById(Integer id) {
+        return productRepository.findOne(id);
+    }
+
+    @Override
+    public Product saveOrUpdate(Product domainObject) {
+        return productRepository.save(domainObject);
+    }
+
+    @Override
+    public void delete(Integer id) {
+        productRepository.delete(id);
+    }
+}
