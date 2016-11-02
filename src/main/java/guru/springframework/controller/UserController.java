8de30bdeeaf63b3955c91f326006ae61383b46a6
Dmytro Burdyga <dmytro.burdyga@gmail.com>
2016-11-03 00:17:25 +0200

    Update UserController

--- src/main/java/guru/springframework/controller/UserController.java
+++ src/main/java/guru/springframework/controller/UserController.java
@@ -9,8 +9,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@Controller
 @RequestMapping("/user")
+@Controller
 public class UserController {
 
     private UserService userService;
@@ -32,22 +32,22 @@ public class UserController {
         return "user/show";
     }
 
-    @RequestMapping("/edit/{id}")
-    public String edit(@PathVariable Integer id, Model model) {
-        model.addAttribute("user", userService.getById(id));
-        return "user/userForm";
-    }
-
     @RequestMapping("/new")
     public String newUser(Model model) {
         model.addAttribute("user", new User());
         return "user/userForm";
     }
 
-    @RequestMapping(value = "/", method = RequestMethod.POST)
+    @RequestMapping("/edit/{id}")
+    public String editUser(@PathVariable Integer id, Model model) {
+        model.addAttribute("user", userService.getById(id));
+        return "user/userForm";
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
     public String saveOrUpdateUser(User user) {
         User savedUser = userService.saveOrUpdate(user);
-        return "redirect:/user/show/" + savedUser.getId();
+        return "redirect:user/show/" + savedUser.getId();
     }
 
     @RequestMapping("/delete/{id}")
