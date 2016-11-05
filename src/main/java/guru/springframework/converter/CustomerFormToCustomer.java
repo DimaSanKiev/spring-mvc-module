9376f22f44b5fe201738d2060924bf73d9446302
Dmytro Burdyga <dmytro.burdyga@gmail.com>
2016-11-06 00:44:56 +0200

    Add CustomerFormToCustomer converter

---
+++ src/main/java/guru/springframework/converter/CustomerFormToCustomer.java
@@ -0,0 +1,31 @@
+package guru.springframework.converter;
+
+import guru.springframework.command.CustomerForm;
+import guru.springframework.domain.Address;
+import guru.springframework.domain.Customer;
+import guru.springframework.domain.User;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CustomerFormToCustomer implements Converter<CustomerForm, Customer> {
+
+    @Override
+    public Customer convert(CustomerForm customerForm) {
+        Customer customer = new Customer();
+        customer.setId(customerForm.getCustomerId());
+        customer.setVersion(customerForm.getCustomerVersion());
+        customer.setFirstName(customerForm.getFirstName());
+        customer.setLastName(customerForm.getLastName());
+        customer.setEmail(customerForm.getEmail());
+        customer.setPhoneNumber(customerForm.getPhoneNumber());
+        customer.setBillingAddress(new Address());
+        customer.setShippingAddress(new Address());
+        customer.setUser(new User());
+        customer.getUser().setId(customerForm.getUserId());
+        customer.getUser().setVersion(customerForm.getUserVersion());
+        customer.getUser().setUsername(customerForm.getUserName());
+        customer.getUser().setPassword(customerForm.getPasswordText());
+        return customer;
+    }
+}
